dd949c32e81c986cffa985197a1198b4ea3cb33d
marntext <marntext@gmail.com>
2021-03-02 23:29:17 +0300

    Created mealsApp

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div style={{ backgroundColor: 'brown' }}>
+            <p style={{ color:'white' }}>Designed and Developed By</p>
+            <p style={{ color:'white' }}>MM</p>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+    return (
+        <div style={{backgroundColor: "brown"}}>
+            <h1>Meals</h1>
+            <a style={{margin: '10px', color:'white'}}>Home</a>
+            <a style={{margin: '10px', color:'white'}}>Order</a>
+            <a style={{margin: '10px', color:'white'}}>Contact Us</a>
+        </div>
+    )   
+}
+export default Navbar;
